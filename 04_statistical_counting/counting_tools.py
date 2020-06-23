89051ab7ec3080404990ed95db02d105d9621ce0
Andrew Roberts <clownfragment@gmail.com>
2020-06-22 19:51:51 -0600

    add perms intuition

--- 04_statistical_counting/counting_tools.py
+++ 04_statistical_counting/counting_tools.py
@@ -19,3 +19,32 @@ def perm(n, k):
 '''
 Let's get all the permutations from a list of items
 '''
+# Consider a base 5 number system: 0, 1, 2, 3, 4
+# How many ways can you rearrange those numbers, using each number only once?
+
+def permutations_nP3(base=5):
+    base_5_list = []
+
+    for i in range(base):
+        for j in range(base):
+            for k in range(base):
+                base_5_list.append([i, j, k])
+
+    permutations = []
+
+    for arrangement in base_5_list:
+        is_permutation = True
+
+        for num in arrangement:
+            if arrangement.count(num) > 1:
+                is_permutation = False
+                break
+
+        if is_permutation:
+            permutations.append(arrangement)
+
+    return permutations
+
+
+print(len(permutations_nP3(base=5)))
+print(perm(n=5, k=3))
