f8c46c48520ba34890ccb6632b25631e9837b7f2
Andrew Roberts <clownfragment@gmail.com>
2020-06-27 13:38:35 -0600

    add combs

--- 05_discrete_distributions/binomial/binomial_using_pmf.py
+++ 05_discrete_distributions/binomial/binomial_using_pmf.py
@@ -12,3 +12,8 @@ def combinations(n, k):
     return int( factorial(n) / (factorial(n-k) * factorial(k)))
 
 def binomial_pmf(n, k, p=0.5):
+    return combinations(n, k) * (p**k) * ((1-p)**(n-k))
+
+# There are 8 components in parallel and at least 3 of those components need to operational for a circuit to function. The probability of any given component being operational is 0.7. What is the probability that 3 components are operational?
+
+print(binomial_pmf(8, 3, p=0.7))
