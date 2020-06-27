ab98b383b90d242a1fff7b29e7e8d63d5f91edcb
Andrew Roberts <clownfragment@gmail.com>
2020-06-27 13:19:05 -0600

    complete binom counting

--- 05_discrete_distributions/binomial/binomial_counting.py
+++ 05_discrete_distributions/binomial/binomial_counting.py
@@ -23,7 +23,8 @@
 
 
 def gen_8_bit_binary_with_fors():
-    bin_lst = []
+    bin_lst = {}
+    counter = 0
     for i in range(2):
         for j in range(2):
             for k in range(2):
@@ -32,15 +33,47 @@ def gen_8_bit_binary_with_fors():
                         for n in range(2):
                             for o in range(2):
                                 for p in range(2):
-                                    bin_lst.append([i,j,k,l,m,n,o,p])
+                                    bin_lst[counter] = [i,j,k,l,m,n,o,p]
+                                    counter += 1
     return bin_lst
 
 
-def binomial_distr(list_of_binary):
+# demo of dec to bin algorithm
+# 10
+# [0,]
+# 5
+# [0,1]
+# 2
+# [0,1,0]
+# 1
+# [0,1,0,1]
+# 0
+# ...
+# [0,1,0,1,0,0,0,0] -> [0,0,0,0,1,0,1,0]
+
+def dec_to_bin(dec, n_bits=8):
+    bin_list = []
+    x = dec
+
+    for _ in range(n_bits):
+        bit = x % 2
+        bin_list.append(bit)
+        x //= 2
+
+    return bin_list[::-1]
+
+def get_binary(n_bits=8):
+    bins_d = {}
+    for dec in range(2**n_bits):
+        bins_d[dec] = dec_to_bin(dec, n_bits)
+    return bins_d
+
+
+def binomial_distr(binary_dict):
     binomial_dict = {}
 
-    for lst in list_of_binary:
-        sum_ = sum(lst)
+    for _, v in binary_dict.items():
+        sum_ = sum(v)
         if sum_ not in binomial_dict:
             binomial_dict[sum_] = 0
         binomial_dict[sum_] += 1
@@ -48,13 +81,24 @@ def binomial_distr(list_of_binary):
     return binomial_dict
 
 
-list_of_binary =  gen_8_bit_binary_with_fors()
+d = get_binary()
 
-binom_dict_8_trials = binomial_distr(list_of_binary)
+# for k, v in d.items():
+#     print(f'{k}: {v}')
 
+binomial_dict = binomial_distr(d)
 
-for k, v in binom_dict_8_trials.items():
+for k, v in binomial_dict.items():
     print(f'{k}: {v}')
+
+
+# for_loop_binary =  gen_8_bit_binary_with_fors()
+
+# binom_dict_8_trials = binomial_distr(for_loop_binary)
+
+
+# for k, v in binom_dict_8_trials.items():
+#     print(f'{k}: {v}')
 
 
 
